eb2504ea4d827f2e952f2a23943e5f59be2ab644
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-06-01 20:46:22 +0300

    style dart file added.

--- lib/common/show_model.dart
+++ lib/common/show_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_todo_app/constants/app_style.dart';
 
 class AddNewTaskModel extends StatelessWidget {
   const AddNewTaskModel({
@@ -8,10 +9,37 @@ class AddNewTaskModel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.80,
+      padding: const EdgeInsets.all(30),
+      height: MediaQuery.of(context).size.height * 0.70,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            child: Text(
+              "Yeni Görev Ekle",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+           ),
+          Divider(
+            thickness: 1.2,
+            color: Colors.grey.shade200,
+          ),
+          const Text(
+            "Görev Başlığı",
+            style: AppStyle.headingStyle,
+            ),
+          const TextField(),
+        ],
       ),
     );
   }
