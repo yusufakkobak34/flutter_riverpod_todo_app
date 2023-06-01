03bf105eac1565681c1f1bdc591dce265839decb
Yusuf Akkobak <yusufakkobak34@gmail.com>
2023-06-01 19:40:53 +0300

    directory added.

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'To-Do App With Riverpod SM',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(   ),
       home: null,
     );
   }
